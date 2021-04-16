fd22f3d59ef1071f7e4225f80f514b2b7e1f1a89
cohlerCoded <jeffcohler@cohlercoded.com>
2021-04-16 14:05:16 -0700

    passing down props

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
 import React from "react";
+import Accordion from "./components/Accordion";
+
+const items = [
+  {
+    title: "What is React",
+    content: "React is a lightweight front-end framework.",
+  },
+  {
+    title: "Why use React",
+    content: "React is a favorite library among engineers.",
+  },
+  {
+    title: "How do you use React",
+    content: "You use React by creating components.",
+  },
+];
 
 export default function App() {
-  return <h1>Widgets App</h1>;
+  return (
+    <div>
+      <h1>Widgets App</h1>
+      <Accordion items={items} />
+    </div>
+  );
 }
