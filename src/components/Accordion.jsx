38273fc70ab9c7da1d1eb5c26334d589ba95ec4b
cohlerCoded <jeffcohler@cohlercoded.com>
2021-04-16 14:20:44 -0700

    built and styled an accordion

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Accordion = ({ items }) => {
-  return <h1>{items.length}</h1>;
+  const renderedItems = items.map((item, i) => {
+    return (
+      <React.Fragment key={i}>
+        <div className="title active">
+          <i className="dropdown icon"></i>
+          {item.title}
+        </div>
+        <div className="content active">
+          <p>{item.content}</p>
+        </div>
+      </React.Fragment>
+    );
+  });
+  return <div className="ui styled accordion">{renderedItems}</div>;
 };
 
 export default Accordion;
