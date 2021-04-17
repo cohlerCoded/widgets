c161bbd7f2e54659ff15d2e28936351b2aa254dd
cohlerCoded <jeffcohler@cohlercoded.com>
2021-04-17 11:53:01 -0700

    expand accordion using useState

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -4,24 +4,20 @@ const Accordion = ({ items }) => {
   const [activeIndex, setActiveIndex] = useState(null);
   const titleClicked = (i) => setActiveIndex(i);
   const renderedItems = items.map((item, i) => {
+    const active = i === activeIndex ? "active" : "";
     return (
       <React.Fragment key={i}>
-        <div className="title active" onClick={() => titleClicked(i)}>
+        <div className={`title ${active}`} onClick={() => titleClicked(i)}>
           <i className="dropdown icon"></i>
           {item.title}
         </div>
-        <div className="content active">
+        <div className={`content ${active}`}>
           <p>{item.content}</p>
         </div>
       </React.Fragment>
     );
   });
-  return (
-    <div className="ui styled accordion">
-      {renderedItems}
-      <h1>{activeIndex}</h1>
-    </div>
-  );
+  return <div className="ui styled accordion">{renderedItems}</div>;
 };
 
 export default Accordion;
