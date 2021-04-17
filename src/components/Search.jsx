0d717730823bd78f9c292e5026c6022b0ed3f77a
cohlerCoded <jeffcohler@cohlercoded.com>
2021-04-17 12:20:38 -0700

    text input with hooks

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,5 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Search() {
-  return <div>search</div>;
+  const [term, setTerm] = useState("");
+  console.log(term);
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label htmlFor="search">Enter Search Term</label>
+          <input
+            type="search"
+            name="search"
+            id="search"
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
