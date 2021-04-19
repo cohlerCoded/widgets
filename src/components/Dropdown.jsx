42858c1c90e760a6eb531c8daa675d113154f0d5
cohlerCoded <jeffcohler@cohlercoded.com>
2021-04-19 12:24:43 -0700

    dropdown structure with semantic

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,5 +1,23 @@
 import React from "react";
 
-export default function Dropdown() {
-  return <div>Dropdown</div>;
+export default function Dropdown({ options }) {
+  const renderedOptions = options.map((option) => {
+    return (
+      <div key={option.value} className="item">
+        {option.label}
+      </div>
+    );
+  });
+  return (
+    <div className="ui form">
+      <div className="field">
+        <label className="label">Select a Color</label>
+        <div className="ui selection dropdown visible active">
+          <i className="dropdown icon"></i>
+          <div className="text">Select a Color</div>
+          <div className="menu visible transition">{renderedOptions}</div>
+        </div>
+      </div>
+    </div>
+  );
 }
