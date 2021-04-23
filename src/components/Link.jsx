493af132da28e7a0c987eb371ce2a717a4ae122b
cohlerCoded <jeffcohler@cohlercoded.com>
2021-04-22 22:12:51 -0700

    detect navigation with popstate

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -4,6 +4,8 @@ export default function Link({ className, href, children }) {
   const onClick = (event) => {
     event.preventDefault();
     window.history.pushState({}, "", href);
+    const navEvent = new PopStateEvent("popstate");
+    window.dispatchEvent(navEvent);
   };
 
   return (
--- src/components/Route.jsx
+++ src/components/Route.jsx
@@ -1,3 +1,13 @@
+import { useEffect } from "react";
 export default function Route({ path, children }) {
+  useEffect(() => {
+    const onLocationChange = () => {
+      console.log("location change");
+    };
+    window.addEventListener("popstate", onLocationChange);
+    return () => {
+      window.removeEventListener("popstate", onLocationChange);
+    };
+  }, []);
   return window.location.pathname === path ? children : null;
 }
