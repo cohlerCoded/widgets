44277ff4ff61c180bb45ff215fd98a53af388f7f
cohlerCoded <jeffcohler@cohlercoded.com>
2021-04-22 22:02:33 -0700

    change url with window.history.pushstate

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -3,6 +3,7 @@ import React from "react";
 export default function Link({ className, href, children }) {
   const onClick = (event) => {
     event.preventDefault();
+    window.history.pushState({}, "", href);
   };
 
   return (
