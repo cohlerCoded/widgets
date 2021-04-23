4a1d5f382ac5ca1fa518383dd901f1a9e85f13a8
cohlerCoded <jeffcohler@cohlercoded.com>
2021-04-22 22:22:59 -0700

    command clicks

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -2,8 +2,13 @@ import React from "react";
 
 export default function Link({ className, href, children }) {
   const onClick = (event) => {
+    if (event.metaKey || event.ctrlKey) {
+      return;
+    }
+
     event.preventDefault();
     window.history.pushState({}, "", href);
+
     const navEvent = new PopStateEvent("popstate");
     window.dispatchEvent(navEvent);
   };
