0e1260fffcf43d0a86603abe8a3c2feea307ae8c
cohlerCoded <jeffcohler@cohlercoded.com>
2021-04-22 22:20:01 -0700

    update route

--- src/components/Route.jsx
+++ src/components/Route.jsx
@@ -1,13 +1,14 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 export default function Route({ path, children }) {
+  const [currentPath, setCurrentPath] = useState(window.location.pathname);
   useEffect(() => {
     const onLocationChange = () => {
-      console.log("location change");
+      setCurrentPath(window.location.pathname);
     };
     window.addEventListener("popstate", onLocationChange);
     return () => {
       window.removeEventListener("popstate", onLocationChange);
     };
   }, []);
-  return window.location.pathname === path ? children : null;
+  return currentPath === path ? children : null;
 }
